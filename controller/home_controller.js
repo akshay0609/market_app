baad86533fe2d153e99e5128991eb4b4271b91b5
Akshay Sharma <akshay.sharma@jombay.com>
2021-01-03 09:11:43 +0530

    Added APIs up call for heroku

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,26 +1,38 @@
 app.controller('home-controller',['$scope', '$rootScope', 
-							'$state','AuthenticationService',
-							 function($scope, $rootScope, $state, AuthenticationService) {
+							'$state','AuthenticationService', 'ProductService',
+							 function($scope, $rootScope, $state, AuthenticationService, ProductService) {
 	
-$scope.images = [
-    {
-      name: "Mobiles",
-      image: 'https://res.cloudinary.com/dzrgwhm1s/image/upload/v1609568563/public/market-app/index-bg-new.jpg'
-    },
-    {
-      name: "Games",
-      image: 'https://res.cloudinary.com/dzrgwhm1s/image/upload/v1609569026/public/market-app/wp3025432.jpg'
-    },
-    {
-      name: "Camera",
-      image: 'https://res.cloudinary.com/dzrgwhm1s/image/upload/v1609572029/public/market-app/48-485878_camera-wallpapers-hd.jpg'
-    },
-    {
-      name: "Hard Drives",
-      image: 'https://res.cloudinary.com/dzrgwhm1s/image/upload/v1609568909/public/market-app/WDBBKD0030BBK-PESN-banner.jpg'
-    }
-  ];
+  $scope.images = [
+      {
+        name: "Mobiles",
+        image: 'https://res.cloudinary.com/dzrgwhm1s/image/upload/v1609568563/public/market-app/index-bg-new.jpg'
+      },
+      {
+        name: "Games",
+        image: 'https://res.cloudinary.com/dzrgwhm1s/image/upload/v1609569026/public/market-app/wp3025432.jpg'
+      },
+      {
+        name: "Camera",
+        image: 'https://res.cloudinary.com/dzrgwhm1s/image/upload/v1609572029/public/market-app/48-485878_camera-wallpapers-hd.jpg'
+      },
+      {
+        name: "Hard Drives",
+        image: 'https://res.cloudinary.com/dzrgwhm1s/image/upload/v1609568909/public/market-app/WDBBKD0030BBK-PESN-banner.jpg'
+      }
+    ];
 
-	$scope.home = $rootScope.login
+  $scope.home = $rootScope.login
 
+  $scope.apis_up = function() {
+    product_index({limit: 1})
+  }
+
+  // For Product Call to up the heroku APIs
+  product_index = function(product_params) {
+    ProductService.index(product_params, function(response, status) {
+      if(status.success) {
+        console.log("successfully connected to server")
+      }
+    })
+  }
 }]);
